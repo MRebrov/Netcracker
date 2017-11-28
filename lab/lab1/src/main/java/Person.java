51ad4dd7ab39ddf0d4514fe27ebd44b85f19a9b3
MRebrov <gerdrjre@mail.ru>
2017-11-28 17:26:47 +0300

    added joda-time

--- lab/lab1/src/main/java/Person.java
+++ lab/lab1/src/main/java/Person.java
@@ -1,10 +1,55 @@
-/**
- * Created by lenovo on 15.11.2017.
- */
+import org.joda.time.LocalDate;
+
 public class Person {
-    private String fio;
-    private String id;
-    private LocalDate birthday;
 
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private int id;
+    private LocalDate birthday = new LocalDate();
 
+    public int getAge(){
+        LocalDate timeNow = new LocalDate();
+        return timeNow.now().getYear() - birthday.getYear();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
 }
